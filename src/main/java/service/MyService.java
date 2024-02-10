d26e6a3598b2fff115a289cea20de42be04db9b8
yogesh chanekar@gmail.com <yogeshchanekar@gmail.com>
2024-02-11 01:31:29 +0530

    for merge purpose

--- src/main/java/service/MyService.java
+++ src/main/java/service/MyService.java
@@ -10,6 +10,8 @@ public class MyService {
 
     // test commit2
 
+    // code added for merging purposes merge1
+
     String myurl = "https://jgentes-crime-data-v1.p.rapidapi.com/crime?startdate=9%2F19%2F2015&enddate=9%2F25%2F2015&long=-122.5076392&lat=37.757815";
 
     public MyService(WebClient.Builder webClientBuilder) {
